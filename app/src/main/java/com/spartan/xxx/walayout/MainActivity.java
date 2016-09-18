7b473114a7ec67f829a9dedcb98f5736453ba99b
fajarachmadyusup <iniemailfajar@gmail.com>
2016-09-18 20:28:18 +0700

    logo baru

--- app/src/main/java/com/spartan/xxx/walayout/MainActivity.java
+++ app/src/main/java/com/spartan/xxx/walayout/MainActivity.java
@@ -1,33 +1,121 @@
 package com.spartan.xxx.walayout;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ProgressBar;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button buttonProfile;
+
+    CircleImageView circleImageView;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        buttonProfile = (Button) findViewById(R.id.buttonProfile);
-
-        buttonProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                callIntent();
-            }
-        });
+        circleImageView = (CircleImageView) findViewById(R.id.fotoLogo);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
     }
 
-    public void callIntent(){
+    public void callIntentProfile(View view){
 
         Intent intent = new Intent(this, Profile.class);
         startActivity(intent);
+    }
+
+    public void callNotification (View view){
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.mipmap.ic_icon)
+                        .setContentTitle("Asprak 4140")
+                        .setContentText("Fajar, TP Mobpro udah beres ?");
+        // Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, ResultActivity.class);
+
+        // The stack builder object will contain an artificial back stack for the
+        // started Activity.
+        // This ensures that navigating backward from the Activity leads out of
+        // your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        // Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(ResultActivity.class);
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        // mId allows you to update the notification later on.
+        mNotificationManager.notify(1, mBuilder.build());
+    }
+
+    public void callLogo (View view){
+        downloadImageFromSource imageFromSource = new downloadImageFromSource();
+        imageFromSource.execute("https://avatars2.githubusercontent.com/u/21212043");
+    }
+
+    public class downloadImageFromSource extends AsyncTask<String, Void, Bitmap>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+
+            String urlStr = params[0];
+            HttpURLConnection urlConnection = null;
+
+
+
+            try {
+
+                URL url = new URL(urlStr);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setDoInput(true);
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                return bitmap;
+
+            }catch (IOException e){
+                e.printStackTrace();
+            }finally {
+                urlConnection.disconnect();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            progressBar.setVisibility(View.INVISIBLE);
+            circleImageView.setImageBitmap(bitmap);
+        }
     }
 }
--- app/src/main/java/com/spartan/xxx/walayout/Profile.java
+++ app/src/main/java/com/spartan/xxx/walayout/Profile.java
@@ -1,12 +1,19 @@
 package com.spartan.xxx.walayout;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -15,6 +22,8 @@ public class Profile extends AppCompatActivity {
     CircleImageView fotoProfil;
     FloatingActionButton camera;
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_IMAGE_GALERI = 2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +31,30 @@ public class Profile extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         fotoProfil = (CircleImageView) findViewById(R.id.fotoProfil);
-        camera = (FloatingActionButton) findViewById(R.id.camera);
 
-        camera.setOnClickListener(new View.OnClickListener() {
+    }
+
+    public void dialogCall(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(Profile.this);
+        builder.setTitle(R.string.title_dialog);
+        builder.setMessage(R.string.text_dialog);
+        builder.setPositiveButton("Kamera", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(DialogInterface dialog, int which) {
                 dispatchTakePictureIntent();
             }
+        }).setNegativeButton("Galeri", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                fotoDariGaleri();
+            }
+        }).setNeutralButton("Batal", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
         });
+        builder.show();
     }
 
     private void dispatchTakePictureIntent(){
@@ -37,6 +62,13 @@ public class Profile extends AppCompatActivity {
         if (takePictureIntent.resolveActivity(getPackageManager()) != null){
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
+    }
+
+    public void fotoDariGaleri(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Pilih Foto"), 2);
     }
 
     @Override
@@ -47,6 +79,41 @@ public class Profile extends AppCompatActivity {
             fotoProfil.setImageBitmap(imageBitmap);
             fotoProfil.setMinimumWidth(275);
             fotoProfil.setMinimumHeight(275);
+
+            View view =  findViewById(R.id.root_view);
+            Snackbar snackbar = Snackbar.make(view, R.string.foto_changed, Snackbar.LENGTH_LONG)
+                    .setActionTextColor(Color.RED)
+                    .setAction("UNDO", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Toast.makeText(getApplicationContext(), R.string.fitur_undo, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+            snackbar.show();
+        }else if(requestCode == REQUEST_IMAGE_GALERI && resultCode == RESULT_OK){
+            Bitmap bitmap = null;
+            if (data != null){
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(
+                            getApplicationContext().getContentResolver(),
+                            data.getData());
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            if(fotoProfil != null){
+                fotoProfil.setImageBitmap(bitmap);
+                View view =  findViewById(R.id.root_view);
+                Snackbar snackbar = Snackbar.make(view, R.string.foto_changed, Snackbar.LENGTH_LONG)
+                        .setActionTextColor(Color.RED)
+                        .setAction("UNDO", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(getApplicationContext(), R.string.fitur_undo, Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                snackbar.show();
+            }
         }
     }
 }
